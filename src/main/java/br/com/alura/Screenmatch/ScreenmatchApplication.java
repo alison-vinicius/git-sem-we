34e42a74e96cf5385bc6e0c2a3c3bb1cfed1c025
Alison Vinicius <viniciussoares024@gmail.com>
2024-07-15 19:26:20 -0500

    inicio do projeto spring

---
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,32 @@
+package br.com.alura.Screenmatch;
+
+import br.com.alura.Screenmatch.model.DadosSerie;
+import br.com.alura.Screenmatch.service.ConsumoApi;
+import br.com.alura.Screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=72b4446d");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+
+
+	}
+}
+//  72b4446d
